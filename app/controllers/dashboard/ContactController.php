3dbb94998485cf87c5416dacbc3ed09371b62626
jonathancg90 <jonathancg90@gmail.com>
2014-02-02 00:30:39 -0500

    contact Controller

--- app/controllers/dashboard/ContactController.php
+++ app/controllers/dashboard/ContactController.php
@@ -7,12 +7,14 @@ use Redirect;
 
 class ContactController extends BaseController {
 
-    public function getListComment(){
+    public function getListComment()
+    {
         $list = Comment::where('status', 1)->paginate(5);
         return View::make('dashboard/message')->with('comments',$list);
     }
 
-    public function getDeleteComment($pk){
+    public function getDeleteComment($pk)
+    {
         $comment = Comment::find($pk);
         $comment->status = 0;
         $comment->save();
