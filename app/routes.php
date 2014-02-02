1ce5cd9a7c14734f8bbea7f0a05013256b8ccfb3
jonathancg90 <jonathancg90@gmail.com>
2014-02-02 01:48:54 -0500

    comment routes

--- app/routes.php
+++ app/routes.php
@@ -38,33 +38,33 @@ Route::group($optionsBack, function()
 {
 
     Route::get('/projects', array('as' => 'api_projects', 'uses' => 'Api\ProjectController@getData'));
-    Route::get('/dashboard', 'HomeController@showDashboard');
-    //Blog
-
-    Route::get('/blog/create', 'blogController@createOneArticle');
-    Route::post('/blog/create', 'blogController@createOneArticle');
-
-    //Messages
-    Route::get('/messages', array('as' => 'admin_messages', 'uses' => 'Dashboard\ContactController@getListComment'));
-    Route::get('/messages/delete/{pk}', array('as' => 'admin_messages_delete', 'uses' => 'Dashboard\ContactController@getDeleteComment'));
-
-    //Projects
-    Route::get('/projects', array('as' => 'admin_projects', 'uses' => 'Dashboard\ProjectController@getListProject'));
-
-    Route::get('/projects/create', array('as' => 'admin_projects_create', 'uses' => 'Dashboard\ProjectController@getFormCreateProject'));
-    Route::post('/projects/save', array('as' => 'admin_projects_save', 'uses' => 'Dashboard\ProjectController@postCreateProject'));
-
-    Route::get('/projects/delete/{pk}', array('as' => 'admin_projects_delete', 'uses' => 'Dashboard\ProjectController@getDeleteProject'));
-    Route::get('/projects/photo/delete/{pk}', array('as' => 'admin_projects_photo_delete', 'uses' => 'Dashboard\ProjectController@getDeletePhoto'));
-
-    Route::get('/projects/update/{pk}', array('as' => 'admin_projects_update', 'uses' => 'Dashboard\ProjectController@getUpdateProject'));
-    Route::put('/projects/update/form/{pk}', array('as' => 'admin_projects_update_form', 'uses' => 'Dashboard\ProjectController@putUpdateProject'));
-    Route::post('/projects/photos/upload/{pk}', array('as' => 'admin_projects_photo_upload', 'uses' => 'Dashboard\ProjectController@postUpload'));
-
-    Route::get('/projects/data-json/{pk}', array('as' => 'admin_projects_data_json', 'uses' => 'Dashboard\ProjectController@getData'));
-    Route::get('/projects/data-json/photos/{pk}', array('as' => 'admin_projects_data_photo_json', 'uses' => 'Dashboard\ProjectController@getPhotos'));
-
-
+//    Route::get('/dashboard', 'HomeController@showDashboard');
+//    //Blog
+//
+//    Route::get('/blog/create', 'blogController@createOneArticle');
+//    Route::post('/blog/create', 'blogController@createOneArticle');
+//
+//    //Messages
+//    Route::get('/messages', array('as' => 'admin_messages', 'uses' => 'Dashboard\ContactController@getListComment'));
+//    Route::get('/messages/delete/{pk}', array('as' => 'admin_messages_delete', 'uses' => 'Dashboard\ContactController@getDeleteComment'));
+//
+//    //Projects
+//    Route::get('/projects', array('as' => 'admin_projects', 'uses' => 'Dashboard\ProjectController@getListProject'));
+//
+//    Route::get('/projects/create', array('as' => 'admin_projects_create', 'uses' => 'Dashboard\ProjectController@getFormCreateProject'));
+//    Route::post('/projects/save', array('as' => 'admin_projects_save', 'uses' => 'Dashboard\ProjectController@postCreateProject'));
+//
+//    Route::get('/projects/delete/{pk}', array('as' => 'admin_projects_delete', 'uses' => 'Dashboard\ProjectController@getDeleteProject'));
+//    Route::get('/projects/photo/delete/{pk}', array('as' => 'admin_projects_photo_delete', 'uses' => 'Dashboard\ProjectController@getDeletePhoto'));
+//
+//    Route::get('/projects/update/{pk}', array('as' => 'admin_projects_update', 'uses' => 'Dashboard\ProjectController@getUpdateProject'));
+//    Route::put('/projects/update/form/{pk}', array('as' => 'admin_projects_update_form', 'uses' => 'Dashboard\ProjectController@putUpdateProject'));
+//    Route::post('/projects/photos/upload/{pk}', array('as' => 'admin_projects_photo_upload', 'uses' => 'Dashboard\ProjectController@postUpload'));
+//
+//    Route::get('/projects/data-json/{pk}', array('as' => 'admin_projects_data_json', 'uses' => 'Dashboard\ProjectController@getData'));
+//    Route::get('/projects/data-json/photos/{pk}', array('as' => 'admin_projects_data_photo_json', 'uses' => 'Dashboard\ProjectController@getPhotos'));
+//
+//
 
 });
 
