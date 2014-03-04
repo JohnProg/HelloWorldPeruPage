befa6929e087bd5012e0638c5cd589b54f035e79
jonathancg90 <jonathancg90@gmail.com>
2014-03-03 20:35:24 -0500

    add errors

---
+++ app/views/404.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Hello World Peru</title>
+    <style>
+        body {
+            background: url(img/404.jpg) no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
---
+++ app/views/500.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Hello World Peru</title>
+    <style>
+    <style>
+        body {
+            background: url(img/500.jpg) no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
